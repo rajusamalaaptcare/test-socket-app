11d607ebec19548a36c219f81dbbf6c073540037
Raju Samala <sraju1397@live.com>
2024-04-27 15:51:00 +0530

    created server app

---
+++ server-app/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const http = require('http');
+const WebSocket = require('ws');
+
+const app = express();
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+wss.on('connection', function connection(ws) {
+  console.log('Client connected');
+
+  ws.on('message', function incoming(message) {
+    console.log('Received:', message);
+    // Broadcast received message to all clients
+    wss.clients.forEach(function each(client) {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(message);
+      }
+    });
+  });
+});
+
+server.listen(8080, function() {
+  console.log('Server is listening on port 8080');
+});
